2592f50f92daec901fecb1407f1a0a84deabfc53
Satwika Kapilavai <69619402+Satwika2910@users.noreply.github.com>
2022-07-07 17:17:18 +0530

    task_5.3

---
+++ UserDetails/Users.js
@@ -0,0 +1,27 @@
+const users = {
+    "users" : [
+        {
+            id:"123",
+            login:"luffy",
+            password:"Password1",
+            age:20,
+            isDeleted : false
+        },
+        {
+            id:"456",
+            login:"zoro",
+            password:"Password2",
+            age:21,
+            isDeleted : false
+        },
+        {
+            id:"789",
+            login:"law",
+            password:"Password3",
+            age:22,
+            isDeleted : false
+        },
+    ]
+} ;
+
+module.exports = users;
